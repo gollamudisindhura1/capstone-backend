09863c79fce8a3ce16d85a4f9cb6bcbcb9d94be1
Sindhura Gollamudi <gollamudisindhura1@gmail.com>
2026-01-29 12:32:23 -0500

    created server.js the entry point for backend and the serving is running

---
+++ server.js
@@ -0,0 +1,35 @@
+// server.js - Main entry point of our backend application
+
+//1. Importing required packages
+// 2. Load environment variables from .env file
+require ('dotenv').config();
+const express = require ('express');  // Web framework for Node.js
+const mongoose = require('mongoose'); // ODM to work with MongoDB easily
+const dotenv = require('dotenv'); // Loads variables from .env file
+const cors = require('cors') // Allows frontend (different origin) to make requests
+const PORT = process.env.PORT || 3000
+const app = express()
+
+//Database Connection
+//connectDB()
+
+// Middleware
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+//  Basic test route - to check if server is alive
+app.get('/', (req, res) => {
+  res.json({ message: 'Pro-Tasker Backend is running!' });
+});
+
+// Enable CORS so React frontend (localhost:5173 or deployed) can talk to backend
+app.use(cors({
+  origin: '*', 
+  credentials: true
+}));
+
+//  Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
+});
